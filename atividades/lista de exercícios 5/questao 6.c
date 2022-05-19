11404dc0526d056b6e63348e0c20a010602b1971
Kauã Nogueira <81389414+kauaumnougueira@users.noreply.github.com>
2022-05-19 17:56:46 -0300

    Update questao 6.c

--- atividades/lista de exercícios 5/questao 6.c
+++ atividades/lista de exercícios 5/questao 6.c
@@ -3,7 +3,7 @@
 int main(){
     int num = 0, result = 0;
 
-    printf("Digite um número de 1 a 10: ");
+    printf("Digite um nÃºmero: ");
     scanf("%d", &num);
 
     for(int i = 1; i <= 10; i++){
